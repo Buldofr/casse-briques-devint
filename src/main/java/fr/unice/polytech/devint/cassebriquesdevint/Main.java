9a4d555c7b3930e83e45ab94176ee519ec2fc61b
Loïc GAILLARD <gaillard.log@gmail.com>
2014-03-05 11:15:05 +0100

    Configuration Maven

---
+++ src/main/java/fr/unice/polytech/devint/cassebriquesdevint/Main.java
@@ -0,0 +1,10 @@
+package fr.unice.polytech.devint.cassebriquesdevint;
+
+/**
+ * Created by user on 05/03/14.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("...");
+    }
+}
